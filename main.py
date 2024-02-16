ba06fc0e644c09f49390d3454cfcfac86bb13c4f
Murat Kaynarca <e203100@metu.edu.tr>
2024-02-17 00:23:05 +0300

    readability improvement

--- main.py
+++ main.py
@@ -4,140 +4,139 @@ Created on Thu Sep  9 09:26:19 2021
 
 @author: alpaylan, askafkas, mkaynarca, nbyavuz
 """
+import colorama
+import random
 
-
-from operator import eq
-import string
-from typing import Tuple
-from venv import create
-
-def confirmation(words:list) -> list:
-    
-    for i in range(len(words)):
-        print(i+1, " : ", words[i], "\n")
-    
-    inp = input("\nIs the word list true? (y/n)\n")
-    
-    if (inp == "y" or inp == "Y"):
-        return words
-    
-    else:
-        inp = input("\nWould you like to edit the list or start over (e for edit/a for add/s sor start over)\n")
-        if (inp == "e" or inp == "E"):
-            editWords(words)
-        elif (inp == "a" or inp == "A"):
-            addWords(words)
-        else:
-            words = createWords()
-            
-
-def editWords(words:list) -> list:
-    edit_mode = True
-    
-    while edit_mode: 
-        for i in range(len(words)):
-            print(i+1, " : ", words[i],  "\n")
-
-        id = int(input("\nPlease enter the ID of the word you would like to edit\n"))
-
-        word = str(input("\nPlease type in the correct word\n"))
-
-        words[id - 1] = word
-        
-        inp = input("\nWould you like to edit more words (y/n)")
-        if (inp == "y" or inp == "Y"):
-            editWords(words)
-            break
-        else:
-            confirmation(words)
-            break
-        
-        
-    
-def addWords(words:list) -> list:
-    word = input("\nPlease type in the new word\n")
-
-    words.append(word)
-    
-    inp = input("\nWould you like to add more words (y/n)")
-    if (inp == "y" or inp == "Y"):
-        editWords(words)
-    else:
-        confirmation(words)
-    
-
-    
-def createWords() -> list:
-    empty_lines = 0
+def testBench(numWords: int, wordLength: int):
     words = []
-    inp = input("\nPlease type in your words\nSubmit empty line to stop\n")   
-    while empty_lines == 0:
-        if inp == "":
-            empty_lines=1
-            confirmation(words)
-        else :
-            words.append(inp)
-            inp = input("")         
-    return words
-                     
-
-def createMatrix(words: list) -> list:
-    similarityMatrix = []
-    for word in words:
-        similarityMatrix.append([])
-        for similar_word in words:
-            similarityMatrix[-1].append(sum(map(eq, word, similar_word)))
-    return similarityMatrix 
-
-def createScore(words: list , similarityMatrix: list) -> list:
-    possibilityMatrix = []
-    for i in range(len(similarityMatrix)):
-        possibilityMatrix.append([])
-        for j in (range(0,len(words[0]) + 1)):
-            possibilityMatrix[-1].append(((similarityMatrix[i].count(j)) * (len(similarityMatrix) - similarityMatrix[i].count(j)))/len(similarityMatrix))
-    scoreMatrix = []
-    for i in range(len(possibilityMatrix)):
-        scoreMatrix.append([])
-        scoreMatrix[-1] = sum(possibilityMatrix[i])
-    return scoreMatrix   
-
-def calculateSelected(words:list , scoreMatrix: list) -> Tuple[int, list]:
-    selectedIndex = scoreMatrix.index(max(scoreMatrix))
-    return selectedIndex, words[selectedIndex]
-
-def simLevel(selected: list) -> int:
-    inp = input(f"\nPlease input the similarity level for {selected}\nLeave empty for exact match\n")    
-    if inp == "":
-        likeness = len(selected)
-    else : 
-        likeness = int(inp)        
-    return likeness
-
-def calculateRemaining(words: list, selected: list, likeness: int) -> list:
-    return [words[i] for i, elem in enumerate(selected) if likeness == elem]
-
-
-def solve(words: list) -> int:
-    remaining = len(words)
-    tour_count = 1
+    for i in range(numWords):
+        words.append("".join([chr(random.randint(97, 122)) for i in range(wordLength)]))
+    
+    password = words[random.randint(0, numWords - 1)]
+    print(f"Password: {password}")
+    tour = 1
     while len(words) > 1:
-        similarityMatrix = createMatrix(words)
-        scoreMatrix = createScore(words, similarityMatrix)
-        selectedIndex, selected = calculateSelected(words,scoreMatrix)        
-        print(f'\nTour {tour_count}')
-        print(f'Selected = {selected}')
-        likeness=simLevel(selected)
-        words = calculateRemaining(words, similarityMatrix[selectedIndex], likeness)
-        print(f'Remaining count = {len(words)}\n')
-        tour_count = tour_count + 1
+        print(f"Tour {tour}")
+        simMatrix = createMatrix(words)
+        posMatrix = createPos(simMatrix)
+        scoreMatrix = createScore(posMatrix)
+        index = bestIndex(scoreMatrix)
+        print(f"Best word: {words[index]}\n")
+        likeness = compareWords(words[index], password)
+        print("\n")
+        words = eliminateWords(words, index, likeness)
+        tour = tour + 1
+        print(f"Remaining words: {len(words)+1}\n")
     if len(words) == 1:
-        print(f'Password cracked, password = {words[0]}')
+        print(f"The password is: {words[0]}")
+        print(f"Tour count: {tour}")
+    
+
+
+def createWords():
+    confirmed = False
+    while not confirmed:
+        inp = input("Write down the words you would like to use, seperated by a comma, spaces are allowed\n")
+        words = inp.replace(" ", "").split(",")
+        if checkWordlist(words):
+            inp = input("Are you sure you want to use these words? (y/n)\n")
+            if inp.lower() == "y":
+                confirmed = True
+        else:
+            print(colorama.Fore.RED + "You fucked up")
+            print(colorama.Fore.RESET)
+    return words
+
+
+def checkWordlist(words: list):
+    if len(words) != len(set(words)):
+        return False
+    length = len(words[0])
+    for word in words:
+        if len(word) != length:
+            return False
+    return True
+
+def compareWords(word1: str, word2: str):
+    sim = 0
+    for i, c in enumerate(word1):
+        if c == word2[i]:
+            sim = sim + 1
+    return sim
+                
+def createMatrix(words: list):
+    if checkWordlist(words):
+        simMatrix = []
+        for tWord in words:
+            simMatrix.append([])
+            for sWord in words:
+                sim = 0
+                for i,c in enumerate(sWord):
+                    if c == tWord[i]:
+                        sim = sim + 1
+                simMatrix[-1].append(sim)
+        return simMatrix
+    else:
+        print("The word list is not valid.")
+        words = createWords()
+        createMatrix(words)
+
+def createPos(simMatrix: list):
+    posMatrix = []
+    for simList in simMatrix:
+        posMatrix.append([])
+        simCounts = dict()
+        for i in simList:
+            simCounts[i] = simCounts.get(i, 0) + 1
+        for i in range(max(simList) + 1):
+            posMatrix[-1].append(simCounts.get(i, 0))
+    return posMatrix
+
+def createScore(posMatrix: list):
+    scoreMatrix = []
+    for posList in posMatrix:
+        score = 0
+        for pos in posList:
+            score = score + pos*(sum(posList) - pos)
+        scoreMatrix.append(score)
+    return scoreMatrix
+
+
+def bestIndex(scoreMatrix: list):
+    return scoreMatrix.index(max(scoreMatrix))
+
+def getLikeness():
+    return int(input("Please enter the likeness level: "))
+               
+
+def eliminateWords(words: list, index: int, likeness: int):
+    remaining = []
+    for selected in words:
+        if compareWords(selected, words[index]) == likeness:
+            remaining.append(selected)
+    return remaining
+
+def solve(words: list):
+    while len(words) > 1:
+        simMatrix = createMatrix(words)
+        posMatrix = createPos(simMatrix)
+        scoreMatrix = createScore(posMatrix)
+        index = bestIndex(scoreMatrix)
+        print(f"Best word: {words[index]}\n")
+        likeness = getLikeness()
+        print("\n")
+        words = eliminateWords(words, index, likeness)
+    if len(words) == 1:
+        print(colorama.Fore.GREEN + f"The password is: {words[0]}")
+        print(colorama.Fore.RESET)
+    else:
+        print(colorama.Fore.RED + "You fucked up.")
+        print(colorama.Fore.RESET)
         
 def main():
     words = createWords()
     solve(words)
-    
-    
+
 if __name__ == "__main__":
     main()
     
